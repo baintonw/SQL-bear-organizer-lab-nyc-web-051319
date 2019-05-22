36ba2b518b922456122833bb76d2bcc407d51376
Will Bainton <baintonw@gmail.com>
2019-05-22 11:16:04 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,39 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 20, "M", "dark brown", "calm", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 10, "M", "black", "intense", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 6, "F", "dark brown", "Nice", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sargeant Brown", 19, "M", "Green", "Slimy", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 13, "F", "dark brown", "goofy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 2, "M", "Black", "Grinchy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 6, "F", "Blue", "naive", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 20, "M", "black", "aggressive", 0);
+
+-- (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+-- (2,"Rowdy", 10, "M", "black", "intense", 1),
+-- (3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
+-- (4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
+-- (5,"Melissa", 13, "F", "dark brown", "goofy", 1),
+-- (6,"Grinch", 2, "M", "Black", "Grinchy", 1),
+-- (7,"Wendy", 6, "F", "Blue", "naive", 1),
+-- (8,null, 20, "M", "black", "aggressive", 0);
+
+-- INSERT INTO bears (name, gender, color) VALUES ("Mr. Chocolate", "M", "Brown");
+-- INSERT INTO bears (name, gender, color) VALUES ("Rowdy", "M", "Grey");
+-- INSERT INTO bears (name, gender, color) VALUES ("Tabitha", "F", "Brown");
+-- INSERT INTO bears (name, gender, color) VALUES ("Sergeant Brown", "M", "Brown");
+-- INSERT INTO bears (name, gender, color) VALUES ("Melissa", "F", "White");
+-- INSERT INTO bears (name, gender, color) VALUES ("Grinch", "M", "Green");
+-- INSERT INTO bears (name, gender, color) VALUES ("Wendy", "F", "White");
+-- INSERT INTO bears (name, gender, color) VALUES (NULL, NULL, "Blue");
+
+
+
+
+
+-- Mr. Chocolate
+-- Rowdy
+-- Tabitha
+-- Sergeant Brown
+-- Melissa
+-- Grinch
+-- Wendy
+-- unnamed (the bear that killed Tim didn't have a name; refer back to how to create a record that doesn't have one value)
